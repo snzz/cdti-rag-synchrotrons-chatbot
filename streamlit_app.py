d4d0b19e0cc118ecaf72bff1c8c9096faabcd8c1
snzz <ikualkov013@gmail.com>
2025-01-08 18:25:13 +0700

    openai update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ if 'responses' not in st.session_state:
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai.api_key)
+llm = ChatOpenAI(model_name="gpt-4o", openai_api_key=openai.api_key)
 
 if 'buffer_memory' not in st.session_state:
     st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
