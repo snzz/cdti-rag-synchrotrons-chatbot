6109d3f7cf7b5ed484b2b1c3eaadc65ec642fd76
snzz <ikualkov013@gmail.com>
2025-01-08 17:13:54 +0700

    opeanai update

--- utils.py
+++ utils.py
@@ -37,11 +37,7 @@ def query_refiner(conversation, query):
             {"role": "user",
              "content": f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:"},
         ],
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
+        temperature=0,
     )
 
     return response.choices[0].message.content
