7e202384935c280717f6c2afb60affc852d06a5e
snzz <ikualkov013@gmail.com>
2025-01-12 00:25:32 +0700

    format formulas

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,4 +96,4 @@ with response_container:
         for i in range(len(st.session_state['responses'])):
             message(st.session_state['responses'][i], key=str(i))
             if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["requests"][i], is_user=True, avatar_style='adventurer-neutral', key=str(i) + '_user')
