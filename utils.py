05b7b27f613084e448101dbd9286b8c82fc4516d
snzz <ikualkov013@gmail.com>
2025-01-12 17:08:02 +0700

    format formulas

--- utils.py
+++ utils.py
@@ -14,11 +14,10 @@ index = pc.Index(name='synchrotrons-index', host='https://synchrotrons-index-qds
 
 def format_math_expressions(text):
     # Убираем квадратные скобки и обрамляем содержимое в $ (для выражений на нескольких строках)
-    text = re.sub(r'\[\s*(.*?)\s*\]', r' \n$\$\n ', text, flags=re.DOTALL)
+    text = re.sub(r'\[\s*(.*?)\s*\]', r' \n$\1$\n ', text, flags=re.DOTALL)
     text = re.sub(r'\n(?=.*\$)', '', text)
     # Удаление спец символов
     text = re.sub(r'[^\w\s\t\r\b]*(?=\$[^\$]*$)', '', text)
-    text = re.sub(r'\\\$', r'$', text)
     # Убираем пробелы перед и после $ внутри формулы, только если пробелы стоят до или после символов $
     text = re.sub(r'\s*\$(.*?)\s*\$', r'$\1$', text)
     text = re.sub(r'\s+\$(?=\S)', '$', text)
