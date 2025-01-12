cb09c8e44a27f8c5cd0b4014481548b2bcdb3c95
snzz <ikualkov013@gmail.com>
2025-01-12 17:11:19 +0700

    format formulas

--- utils.py
+++ utils.py
@@ -14,7 +14,8 @@ index = pc.Index(name='synchrotrons-index', host='https://synchrotrons-index-qds
 
 def format_math_expressions(text):
     # Убираем квадратные скобки и обрамляем содержимое в $ (для выражений на нескольких строках)
-    text = re.sub(r'\[\s*(.*?)\s*\]', r' \n$\1$\n ', text, flags=re.DOTALL)
+    # text = re.sub(r'\[\s*(.*?)\s*\]', r' \n$\1$\n ', text, flags=re.DOTALL)
+    text = re.sub(r'\\\[|\\\]', '$\n', text, flags=re.DOTALL)
     text = re.sub(r'\n(?=.*\$)', '', text)
     # # Удаление спец символов
     # text = re.sub(r'[^\w\s\t\r\b]*(?=\$[^\$]*$)', '', text)
