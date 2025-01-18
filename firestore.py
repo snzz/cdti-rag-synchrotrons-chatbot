bf39f1012ec340e20db81d10718d37ff661e0b83
snzz <ikualkov013@gmail.com>
2025-01-18 12:25:38 +0700

    update firestore credentials load logic

--- firestore.py
+++ firestore.py
@@ -20,7 +20,7 @@ from firebase_admin import credentials, firestore
 #     with open(cred_json_file_path, 'w') as f:
 #         f.write(fs_cred_str)
 
-fs_cert = credentials.Certificate('credentials.json')
+fs_cert = credentials.Certificate('cdti-rag-synchrotrons-firebase-adminsdk-7jfem-57151483b0.json')
 firebase_admin.initialize_app(fs_cert)
 fs = firestore.client()
 
