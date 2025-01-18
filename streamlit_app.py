04487167a6732ed2675b1beb87ecbe958f2b2314
snzz <ikualkov013@gmail.com>
2025-01-18 11:59:13 +0700

    update firestore credentials load logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ os.environ['OPENAI_API_KEY'] = st.secrets["general"]["OPENAI_API_KEY"]
 index_name = 'synchrotrons-index'
 llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
 fs_cred_str = re.sub(r"'", '"', fs_cred_str)
-fs_cred_str = re.sub(r'\\n', '\\\\n', fs_cred_str)
+fs_cred_str = fs_cred_str.replace("\n", "\\n")
 st.write(fs_cred_str)
 
 if 'buffer_memory' not in st.session_state:
