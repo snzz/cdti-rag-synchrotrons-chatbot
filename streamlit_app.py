5d36daa97de38b9ae0712131d8b74202f995f0e7
snzz <ikualkov013@gmail.com>
2025-02-14 14:48:26 +0700

    back to older chat version

--- streamlit_app.py
+++ streamlit_app.py
@@ -305,8 +305,6 @@ with textcontainer:
 with response_container:
     if st.session_state['responses']:
         for i in range(len(st.session_state['responses'])):
-            with st.chat_message(name='ai'):
-                st.write(st.session_state['responses'][i])
-                if i < len(st.session_state['requests']):
-                    with st.chat_message(name='user'):
-                        st.write(st.session_state["requests"][i])
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
