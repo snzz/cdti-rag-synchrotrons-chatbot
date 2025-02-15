8bed6f4d2149dd01372b8330b5984367764af5d9
snzz <ikualkov013@gmail.com>
2025-02-15 14:33:31 +0700

    fix source file history name format

--- streamlit_app.py
+++ streamlit_app.py
@@ -294,6 +294,7 @@ with textcontainer:
             answer += '\n\n**Источники:**'
             for doc in response["source_documents"]:
                 doc_str = f'\n- {doc.metadata.get('source', 'Неизвестный источник')}'
+                doc_str = os.path.basename(doc_str)
                 if doc_str not in source_docs:
                     source_docs.append(doc_str)
                     answer += doc_str
